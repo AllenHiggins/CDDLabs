6162f66193347511ff5ad8f51d16fd94e3b8de61
allen <allenghiggins@gmail.com>
2017-10-04 22:18:31 +0100

    Docs updated

--- lab2/docs/html/search/all_4.js
+++ lab2/docs/html/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['taskone',['taskOne',['../ronz_8cpp.html#a2696073bbedb6b55e6a52d2180c2a9b3',1,'ronz.cpp']]],
-  ['tasktwo',['taskTwo',['../ronz_8cpp.html#ae4ea9570be601d182fa473c7ca431852',1,'ronz.cpp']]]
+  ['taskone',['taskOne',['../ronz_8cpp.html#aa4d8446950451643863852e525143027',1,'ronz.cpp']]],
+  ['tasktwo',['taskTwo',['../ronz_8cpp.html#a936e0443086bc76ddf2bc31dd61f6e86',1,'ronz.cpp']]]
 ];
