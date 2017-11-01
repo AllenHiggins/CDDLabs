10861d6f039a38cc25df17aac3b70cc385ad0de8
allen <allenghiggins@gmail.com>
2017-11-01 15:17:28 +0000

    init lab 5

---
+++ lab5/producer_consumer.cpp
@@ -0,0 +1,45 @@
+// producer_consumer.cpp --- 
+// 
+// Filename: producer_consumer.cpp
+// Description: A small programe to show to slove the producer consumer problem
+// Author: Allen Higgins
+// Maintainer: 
+// Created: Wed Nov  1 15:09:19 2017 (+0000)
+// Version: 0.0.1
+// 
+// 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 
+
+// Code:
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+
+
+
+
+int main(void)
+{
+
+
+
+  
+  return 0;
+}
+
+// 
+// producer_consumer.cpp ends here
